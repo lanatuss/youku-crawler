9ed59f9b537f86e48c4f64f0466230fb7125de6b
alex312 <lixuanalex123@163.com>
2018-07-31 23:56:55 +0800

    search from from youku vidoe search page

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# from urllib.request import urlopen
-# from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+
 import requests
 from requests.compat import urljoin, urlencode
 
-
 START_URL = "http://www.soku.com/search_video/"
 
-
 def createSearchURL(key, prefix='q_'):
-    key = prefix + urlencode(key)
+    key = prefix + key
     return urljoin(START_URL, key)
+
+def search_from_youku_video(key):
+    url = createSearchURL(key)
+    r = requests.get(url)
+    return r
+
+if __name__ == "__main__":
+    bs_obj = BeautifulSoup(search_from_youku_video("包青天"))
