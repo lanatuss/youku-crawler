36908553568ea4427c427575372c5d8b67ba78a2
李轩 <xuan.li@bonintech.com>
2018-07-31 18:04:36 +0800

    update vscode setting,remove task.json,modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
+# from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("http://youku.com/")
-bs_obj = BeautifulSoup(html, features="html.parser")
-print(bs_obj.a)
+import requests
+from requests.compat import urljoin, urlencode
+
+
+START_URL = "http://www.soku.com/search_video/"
+
+
+def createSearchURL(key, prefix='q_'):
+    key = prefix + urlencode(key)
+    return urljoin(START_URL, key)
